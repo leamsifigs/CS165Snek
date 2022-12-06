fb0908049bd4755927eaa3504d0d7840619fe8df
Ismael <leamsifigs@users.noreply.github.com>
2022-12-05 23:43:40 -0800

    makefile + changes

--- main.cpp
+++ main.cpp
@@ -5,24 +5,30 @@ int main (int argc, char* argv[])
 {
     SDL_Init( SDL_INIT_EVERYTHING );
     SDL_Window *window = SDL_CreateWindow("Hellow WOrld", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_ALLOW_HIGHDPI );
+    auto renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Event event;
 
-    if (NULL == window)
-    {
-        std::cout <<"failed to create window" <<SDL_GetError() << std::endl;
-        return 1;
+    bool running = true;
 
-    }
-    SDL_Event windowEvent;
-    while (true)
+    while (running)
     {
-        if (SDL_PollEvent( &windowEvent))
+        //check inputs
+        //
+        while (SDL_PollEvent( &event ) ) 
         {
-            if ( SDL_QUIT == windowEvent.type)
-            {break;}
-
+            if (event.type == SDL_QUIT) {running = false;}
         }
+
+        //move
+
+        //clear window
+        SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+        SDL_RenderClear(renderer);
+
+        //draw body
+        SDL_RenderPresent(renderer);
+        SDL_Delay(25);
     }
-    SDL_DestroyWindow( window );
-    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
